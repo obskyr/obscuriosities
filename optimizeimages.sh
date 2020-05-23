0f804df2973924a7e5b8dc42b49a6495f25b1746
obskyr <powpowd@gmail.com>
2020-05-23 11:39:34 +0900

    Go even harder on PNG optimization.

--- optimizeimages.sh
+++ optimizeimages.sh
@@ -11,6 +11,13 @@ jpegs="$(find $dir -type f -name '*.jpeg') $(find $dir -type f -name '*.jpg')"
 
 if [ ! -z "$pngs" ]
 then
+    # The order here is meaningful: PNGOUT has better detection for mode
+    # conversions that save space, so doing that in advance may let OptiPNG
+    # yield better results.
+    for png in $pngs
+    do
+        pngout.exe $png
+    done
     optipng $pngs
 fi
 
