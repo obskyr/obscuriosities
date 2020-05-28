9def67811dbddf44db0fc7c11b9c3542b74244c0
obskyr <powpowd@gmail.com>
2020-05-28 23:34:01 +0900

    Looks like I got the PNG optimization backwards.

--- optimizeimages.sh
+++ optimizeimages.sh
@@ -11,14 +11,14 @@ jpegs="$(find $dir -type f -name '*.jpeg') $(find $dir -type f -name '*.jpg')"
 
 if [ ! -z "$pngs" ]
 then
-    # The order here is meaningful: PNGOUT has better detection for mode
-    # conversions that save space, so doing that in advance may let OptiPNG
-    # yield better results.
+    # The order here is meaningful: OptiPNG has better detection for mode
+    # conversions that save space, so doing that in advance may let PNGOUT
+    # (which tends to excel in compression) yield better results.
+    optipng $pngs
     for png in $pngs
     do
         pngout.exe $png
     done
-    optipng $pngs
 fi
 
 if [ ! -z "$jpegs" ]
