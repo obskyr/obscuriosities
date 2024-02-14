d6314ce124bb0503dafc1da1ef6cc33b4923e3d4
obskyr <powpowd@gmail.com>
2024-02-14 02:39:51 +0100

    XML escape URLs in the RSS feed; fixed Ogg feed.

--- jekyll-octopod/lib/jekyll/paged_feed_page_generator.rb
+++ jekyll-octopod/lib/jekyll/paged_feed_page_generator.rb
@@ -5,7 +5,8 @@ module Jekyll
     def generate(site)
       pages_total = (site.posts.docs.count.to_f / site.config["episodes_per_feed_page"]).ceil
 
-      site.config["episode_feed_formats"].each do |page_format|
+      # TODO: Remove the ['ogg'] special case from here once the Ogg feed has been removed.
+      (site.config["episode_feed_formats"] + ['ogg']).each do |page_format|
         name = "episodes." + page_format + ".rss"
         page = PagedFeedPage.new(site, site.source, ".", name, 1, pages_total, page_format)
         site.pages.reject! { |existing_page| existing_page.relative_path == page.relative_path }
